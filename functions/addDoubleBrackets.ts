c86b36847e7809cec400d3b9b415808e462b8688
Damien Schneider <damien.schneider01@gmail.com>
2023-04-23 12:20:38 +0200

    :construction: Prepare the addDoubleBrackets function

---
+++ functions/addDoubleBrackets.ts
@@ -0,0 +1,24 @@
+import * as fs from "fs-extra";
+
+export async function addDoubleBrackets(
+	files: string[],
+	words: string[]
+): Promise<void> {
+	const wordSet = new Set(words);
+	const regexWords = words.map((word) => `\\b${word}\\b`).join("|");
+	const regex = new RegExp(regexWords, "gi");
+
+	for (const file of files) {
+		const content = await fs.readFile(file, "utf-8");
+		const updatedContent = content.replace(regex, (match: any) => {
+			if (wordSet.has(match.toLowerCase())) {
+				return `[[${match}]]`;
+			}
+			return match;
+		});
+
+		if (content !== updatedContent) {
+			await fs.writeFile(file, updatedContent, "utf-8");
+		}
+	}
+}
